bf4f7c216424cef395b6fced5efbd93be6e79370
avery <avry@pm.me>
2021-10-03 15:45:07 -0700

    extended functionality

--- faction.py
+++ faction.py
@@ -6,6 +6,28 @@ class Faction:
     def __init__(self, name, parent=None, children=[], characters=[]):
         self.name, self.parent, self.children, self.characters = \
             name, parent, children, characters
+        self.childrenNames = [x.name for x in self.children]
+
+    def populate(self, characters):
+        self.characters += characters
 
     def makeChild(self, name, children=[], characters=[]):
-        return Faction(name, parent=self, children=children, characters=characters)
+        child = Faction(name, parent=self, children=children, characters=characters)
+        self.children.add(child)
+        self.childrenNames.append(child.name)
+        return child
+
+    def hasChildNamed(self, name):
+        if name in self.childrenNames:
+            return True
+        else:
+            return False
+
+    def show(self, spacing=''):
+        """recursively prints faction and children
+        """
+        if self.children:
+            print(spacing + f"{self.name} contains: {self.childrenNames}")
+        spacing += '    '
+        for child in self.children:
+            child.show(spacing)

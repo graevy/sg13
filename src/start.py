d8952cb0d34a2dfa5b0486d0561ec65f4eda3f05
avery <avrypromo@pm.me>
2021-10-14 16:48:22 -0700

    traceback

--- src/start.py
+++ src/start.py
@@ -1,18 +1,22 @@
 try:
+    from importlib import reload
+    import traceback
+
     import character
     import playertools
     from dmtools import *
     import item
     import resources
 
-    from importlib import reload
     print("Packages imported")
 
-except Exception as e:
-    print(f"Packages failed to import: {e}")
+except Exception:
+    print(f"Packages failed to import:")
+    print(traceback.format_exc())
 
 try:
     factions = load()
     print(f"Factions loaded:\n{factions}")
-except Exception as e:
-    print(f"Factions failed to load: {e}")
+except Exception:
+    print(f"Factions failed to load:")
+    print(traceback.format_exc())

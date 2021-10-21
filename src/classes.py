0a6cb279f7e68a65c3f78c0df87f366d941079aa
avery <avrypromo@pm.me>
2021-10-20 18:32:52 -0700

    bonus stat implementation

--- src/classes.py
+++ src/classes.py
@@ -1,18 +1,18 @@
 def soldier(charObj):
     charObj.hitDie = 10
-    charObj.attributes['constitution'] += 2
-    charObj.skills['tactics'] += 1
-    charObj.skills['athletics'] += 1
+    charObj.bonusAttrs['constitution'] += 2
+    charObj.bonusSkills['tactics'] += 1
+    charObj.bonusSkills['athletics'] += 1
 
 def scientist(charObj):
     charObj.hitDie = 8
-    charObj.attributes['intelligence'] += 3
-    charObj.skills['technology'] += 2
-    charObj.skills['xenotechnology'] += 2
+    charObj.bonusAttrs['intelligence'] += 3
+    charObj.bonusSkills['technology'] += 2
+    charObj.bonusSkills['xenotechnology'] += 2
 
 def anthropologist(charObj):
     charObj.hitDie = 8
-    charObj.attributes['wisdom'] += 3
-    charObj.skills['anthropology'] += 2
-    charObj.skills['xenoanthropology'] += 1
-    charObj.skills['diplomacy'] += 1
+    charObj.bonusAttrs['wisdom'] += 3
+    charObj.bonusSkills['anthropology'] += 2
+    charObj.bonusSkills['xenoanthropology'] += 1
+    charObj.bonusSkills['diplomacy'] += 1

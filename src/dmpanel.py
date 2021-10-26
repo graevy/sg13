ce6f6954ecf1279632ebf385463ccc9c29f01e9e
avery <avrypromo@pm.me>
2021-10-26 14:08:50 -0700

    frame

--- src/dmpanel.py
+++ src/dmpanel.py
@@ -4,10 +4,12 @@ import dmtools
 import inspect
 
 class DMPanel(tk.Tk):
-    def __init__(self):
+    def __init__(self, width=700, height=350):
         super().__init__()
         self.title("SG13 DM Toolkit")
         # self.label = tk.Label(self, text="SG13 DM Toolkit")
+        self.geometry(f"{width}x{350}")
+
 
         # for readability
         dmFns = [fnTuple for fnTuple in inspect.getmembers(dmtools, inspect.isfunction)]
@@ -15,6 +17,9 @@ class DMPanel(tk.Tk):
         
         rollFns = [fnTuple for fnTuple in inspect.getmembers(rolls, inspect.isfunction)]
         self.buttons += [tk.Button(text=fnName, command=fn) for fnName,fn in rollFns]
+
+        # populating the GUI
+        frame = tk.Frame()
         
 
 if __name__ == '__main__':

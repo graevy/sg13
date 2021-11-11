092c6ec87cb4631fea4731bcc3f2e7799c2339d2
avery <avry@pm.me>
2021-11-11 03:02:26 -0800

    first real templates

--- src/npc_templates/template.py
+++ src/npc_templates/template.py
@@ -7,26 +7,29 @@ import os
 
 class Template:
     def __init__(self, name, race, clas, faction, slots=None):
-        if slots is None or slots == {}:
-            self.slots = self.load_slots()
-        else:
-            self.slots = slots
         self.name = name
         self.race = race
         self.clas = clas
         self.faction = faction
 
+        if slots is None:
+            self.slots = {}
+            self.load_slots()
+        else:
+            self.slots = slots
+
     def load_slots(self):
         # race extensibility
-        with open(f".{os.sep}races{os.sep}{self.race}.json", encoding='utf-8') as f:
+        with open(f".{os.sep}races{os.sep}{self.race}{os.sep}{self.race}.json", encoding='utf-8') as f:
             default_slots = json.load(f)['slots']
 
         for slot in default_slots:
-            self.slots[slot] = dmtools.load_item(input(f"{slot}? >>> ")) if input != "" else None
+            s = input(f"{slot}? >>> ")
+            self.slots[slot] = dmtools.load_item(s) if s != "" else None
             
     def save(self, template_dir=f".{os.sep}npc_templates{os.sep}"):
         with open(template_dir + self.name + '.json', 'w+', encoding='utf-8') as f:
-            json.dump(character.Character.new(name=self.name, race=self.race, clas=self.clas, slots=self.slots, faction=self.faction).get_json(), f)
+            json.dump(character.Character.new({'name':self.name, 'race':self.race, 'clas':self.clas, 'slots':self.slots, 'faction':self.faction}).get_json(), f)
 
 # airman = npc_templates.template.Template(name='airman',race='human',clas='soldier',faction='sgc')
 # airman.load_slots()

5395cdd107524806e77b75f1e4ded221a897a4b2
avery <avrypromo@pm.me>
2021-12-05 17:47:24 -0800

    first unit tests?

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,6 +1,12 @@
 import pytest
 import src
 
+char = src.character.Character.create(dict(name='named', race='human', clas='soldier', level=1))
+
 def test_character_creation():
-    char = src.character.Character.create(dict(name='named', race='human', clas='soldier', level=1))
     assert type(char) == src.character.Character
+
+def test_level_up_auto():
+    level_old = char.level
+    char.level_up_auto()
+    assert char.level - level_old == 1

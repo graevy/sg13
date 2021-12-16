9a4e196c9b40f8f22e40a73dc4ee2edd9f91df8c
avery <avry@pm.me>
2021-12-16 13:59:54 -0800

    polish

--- src/cfg/dirs.py
+++ src/cfg/dirs.py
@@ -5,3 +5,22 @@ RACES_DIR = f".{sep}races{sep}"
 CLASES_DIR = f".{sep}clases{sep}"
 ITEMS_DIR = f".{sep}items{sep}"
 TEMPLATES_DIR = f".{sep}npc_templates{sep}"
+
+# # if i ever end up extending things
+# walker = os.walk('.')
+
+# def full_dir(root, directory):
+#     return root + sep + directory + sep
+
+# for root, directory, _ in next(walker):
+#     match directory:
+#         case 'factions':
+#             factions_dir = full_dir(root, directory)
+#         case 'races':
+#             races_dir = full_dir(root, directory)
+#         case 'clases':
+#             clases_dir = full_dir(root, directory)
+#         case 'items':
+#             items_dir = full_dir(root, directory)
+#         case 'templates':
+#             templates_dir = full_dir(root, directory)
